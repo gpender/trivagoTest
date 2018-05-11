66ea11b2c07b30aa94cafb7d11f349bb1ebad1fd
Guy Pender <guy.pender@me.com>
2018-05-11 13:59:24 +0100

    return sample hotel data

--- routes/hotels.js
+++ routes/hotels.js
@@ -6,6 +6,74 @@ module.exports = (app) => {
     console.log(req.body);
     console.log(req.headers);
 
-    res.send('hello');
+const hotelData = {
+"api_version": 4,
+"lang": "en_GB",
+"hotels": [
+{
+"partner_reference": "5568",
+"name": "Hotel Adlon",
+"street": "Unter den Linden 77",
+"city": "Berlin",
+"postal_code": "10117",
+"state": "NRW",
+"country": "Germany",
+"latitude": 52.51624,
+"longitude": 13.380437,
+"desc": "Welcome to the hotel Adlon Kempinski, a hotel full of tradition and elegance in the heart of the city center.",
+"amenities": [
+"Beauty Center",
+"Business Center",
+"Café/ Bistro",
+"Sauna"
+],
+"url": "http://advertiser-site.com/hoteladlon",
+"email": "adlon@adlon.com",
+"phone": "030-123123",
+"fax": "030-12341234",
+"room_types": {
+"Executive Double": {
+"url": "http://advertiser-site.com/hoteladlon/executive_double",
+"desc": "When Lorenz Adlon opened this luxury hotel in 1907, he fulfilled his lifetime dream. The incredible aura of Hotel Adlon embraces you as soon as you cross the door."
+},
+"Superior Deluxe Double": {
+"url": "http://advertiser-site.com/hoteladlon/superior_deluxe_Double",
+"desc": "The view from the window is too spectacular to be ignored."
+}
+}
+},
+{
+"partner_reference": "12345",
+"name": "Hotel Majestic",
+"street": "Viale Milano 18",
+"city": "Rome",
+"postal_code": "00115",
+"state": "Lazio",
+"country": "Italy",
+"latitude": 132.51624,
+"longitude": 10.380437,
+"desc": "Welcome to the hotel Majestic, a hotel full of tradition and elegance in the heart of the city center.",
+"amenities": [
+"Beauty Center",
+"Business Center",
+"Café/ Bistro",
+"Sauna"
+],
+"url": "http://advertiser-site.com/hotelmajestic",
+"email": "majestic@majestic.com",
+"phone": "030-123123",
+"fax": "030-12341234",
+"room_types": {
+"Superior Deluxe Double": {
+"url": "http://advertiser-site.com/hotelmajestic/superior_deluxe_Double",
+"desc": "The view from the window is too spectacular to be ignored."
+}
+}
+}
+]
+};
+
+
+    res.send(JSON.stringify(hotelData));
   })
 }
